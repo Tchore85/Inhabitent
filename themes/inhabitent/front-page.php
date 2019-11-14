91894b83821ba98d87a2bcb6cc434df8601ddb3f
Tchore85 <prokopko1@gmail.com>
2019-11-13 17:22:52 -0800

    loops fot front page picture

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -11,9 +11,34 @@
 
 </div>
 
-<img src="<?php echo get_stylesheet_directory_uri();?>/images/adventure-photos/canoe-girl.jpg">
-<img src="<?php echo get_stylesheet_directory_uri();?>/images/adventure-photos/beach-bonfire.jpg">
+<!-- <img src="<?php echo get_stylesheet_directory_uri();?>/images/adventure-photos/canoe-girl.jpg"> -->
+<!-- <img src="<?php echo get_stylesheet_directory_uri();?>/images/adventure-photos/beach-bonfire.jpg"> -->
+
 </section>
+
+<!-- <section class="selected-posts"> -->
+<div class ="selected-posts-front-page">
+<?php
+
+$args = array( 'post_type' => 'post','posts_per_page' => 3, 'order' => 'DESC' );
+$journal_posts = get_posts( $args ); // returns an array of posts
+?>
+<?php foreach ( $journal_posts as $post ) : setup_postdata( $post ); ?>
+<article class="post">
+<div class="post-image">
+<?php the_post_thumbnail([768,420]);?>
+</div>
+<?php the_title();?>
+<?php the_date();?>
+</article>
+
+<?php //echo get_the_date();?> 
+<!-- this is longer version echo will display but if we have only get_the_date
+we will get only the data but not displayed -->
+<?php endforeach; wp_reset_postdata(); ?>
+
+</div>
+<!-- </section> -->
 
 
 
--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -23,14 +23,10 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
-			<div class ="container">
-			<?php get_search_form();?>
-			<div class="site-branding">
-				<div class="logo">
-					<img src="<?php echo get_stylesheet_directory_uri();?>/images/inhabitent-logo-tent-white.svg">
-</div>
-</div>
-
+				<div class="site-branding">
+					<div class="logo">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="<?php echo get_stylesheet_directory_uri();?>/images/inhabitent-logo-tent-white.svg"></a>
+					</div>
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
@@ -39,6 +35,8 @@
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 				</nav><!-- #site-navigation -->
+
+				<?php get_search_form();?>
 			</header><!-- #masthead -->
 
 			<?php //if ( has_post_thumbnail() ) {
