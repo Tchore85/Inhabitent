2cc2a6bf61ad1790968575e0cde8b25a7bb27dd9
Tchore85 <prokopko1@gmail.com>
2019-11-27 20:57:27 -0800

    correcture of comments in single.php

--- themes/inhabitent/single.php
+++ themes/inhabitent/single.php
@@ -40,7 +40,7 @@
 								
 
 								<div class="entry-meta">
-									<?php red_starter_comment_count(); ?> / <?php red_starter_posted_by(); ?> /<?php red_starter_posted_on(); ?>
+								<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
 							   </div><!-- .entry-meta -->
 							   
 					</div>
